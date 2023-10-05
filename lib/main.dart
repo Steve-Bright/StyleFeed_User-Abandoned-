912a0275fa5086bbe64c83329aa7ac0c3d636e9a
Steve Bright <swemhtet.net@gmail.com>
2023-10-05 10:15:23 +0630

    copied the entire app development user to github

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'signUp.dart';
+import 'signIn.dart';
+
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+     initialRoute: '/signIn',
+    routes: {
+      '/signIn': (context) => const SigningInPage(),
+      '/signUp': (context) => const SigningUpPage(),
+    }
+  )
+);
+
+
+
+
+
