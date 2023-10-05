e719cbdcc4288cf6b6900188f41595cdb39f584e
Steve Bright <swemhtet.net@gmail.com>
2023-10-06 00:28:52 +0630

    cart_progress_2

--- lib/user_pages/cart.dart
+++ lib/user_pages/cart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quantity_input/quantity_input.dart';
+import 'package:style_feed/user_pages/assetPaths.dart';
 
 class Cart extends StatefulWidget {
   const Cart({super.key});
@@ -42,45 +43,6 @@ class CartItems extends StatefulWidget {
 }
 
 class _CartItemsState extends State<CartItems> {
-  int quantity_count = 0;
-
-  final List<Container> assetPaths = [
-    Container(
-      margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
-      child: Row(
-        children: <Widget>[
-          InkWell(
-            onTap: (){},
-            child: Image.asset('assets/items/lacoste.jpg', width: 150, height: 120),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(
-                height: 30,
-                child: Text('Item Name')
-              ),
-              SizedBox(
-                height: 30,
-                child: Text('30,000 Kyats')
-              ),
-              SizedBox(
-                height: 30
-              ),
-              SizedBox(
-                height: 30,
-                child: QuantityInput(
-
-                ),
-              )
-            ]
-          )
-          // Text('item bla bla bla')
-        ]
-      )
-    )
-  ];
-
 
   @override
   Widget build(BuildContext context) {
