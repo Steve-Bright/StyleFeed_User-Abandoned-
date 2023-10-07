9ad577e8cccad1afce2649f83cf3100ef43878e0
MoeYanHtun <moeyan227970@gmail.com>
2023-10-07 20:52:16 +0630

    create Crad

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:style_feed/ProductCradDetail/productDetail.dart';
 import 'LoginPage/signUp.dart';
 import 'LoginPage/signIn.dart';
 import 'Navigation/SavedPage/saveCard.dart';
 import 'Navigation/CartPage/cart.dart';
 
-void main() => runApp(
-  MaterialApp(
-    debugShowCheckedModeBanner: false,
-     initialRoute: '/signIn',
-    routes: {
-      '/signIn': (context) => const SigningInPage(),
-      '/signUp': (context) => const SigningUpPage(),
-      '/savedCard': (context) => const SavedPage(),
-      '/cart': (context) => const Cart(),
-    }
-  )
-);
-
-
-
-
-
+void main() => runApp(MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/productDetail',
+        routes: {
+          '/productDetail': (context) => DetailCrad(),
+          '/signIn': (context) => const SigningInPage(),
+          '/signUp': (context) => const SigningUpPage(),
+          '/savedCard': (context) => const SavedPage(),
+          '/cart': (context) => const Cart(),
+        }));
